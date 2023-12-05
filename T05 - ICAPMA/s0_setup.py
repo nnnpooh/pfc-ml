1b509e20bf303291a77d02d6585ea48014c2cf12
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2023-12-05 22:55:32 +0700

    Update setup

---
+++ T05 - ICAPMA/s0_setup.py
@@ -0,0 +1,19 @@
+import os, sys
+
+cwd = sys.path[0]
+
+folders = [
+    "o1_pfc_test",
+    "o1_pfc_train",
+    "o2_train_data",
+    "o3_checkpoints",
+    "o3_model_params",
+    "o4_analyze",
+]
+
+for folder in folders:
+    folderPath = os.path.join(cwd, folder)
+    if not (os.path.exists(folderPath)):
+        os.mkdir(folderPath)
+    else:
+        print(folderPath, "already exists.")
